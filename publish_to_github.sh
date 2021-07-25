cff3735ada09bec454944da9b53b121c73798bf8
丁科 <me@dingkewz.com>
2021-07-25 11:55:10 +0800

    sync

--- publish_to_github.sh
+++ publish_to_github.sh
@@ -27,4 +27,4 @@ echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
 echo "Pushing to Github"
-git push origin gh-pages
+git push --force origin gh-pages
